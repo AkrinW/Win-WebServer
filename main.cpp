b449cac7ba0ee228b2f9dd9f0a8e9d9bf6b0fe80
akarinLaptop <791972041@qq.com>
2024-06-16 20:27:51 +0800

    link ws2_32

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
+#include "winsock2.h"
 #include <iostream>
-#include <winsock2.h>
-
+// #pragma comment(lib, WS2_32)
 
 int main() {
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        return 1;
+    }
+    // 你的网络编程代码
+    WSACleanup();
     return 0;
 }
